6f405012002e8cc0c56236bc1529ebcb595411f3
Manerova Olga <o.manerova@qsoft.ru>
2021-12-26 21:05:25 +0300

    Задание 21 урока

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import timer from './modules/timer'
 import menu from './modules/menu'
 import modal from './modules/modal'
+import validate from './modules/validate'
 
 timer('20 december 2021');
 menu();
 modal();
+validate();
